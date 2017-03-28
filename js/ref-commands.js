b65d582065c58c69432014201ebfa656ac783eb5
xazrad <xazrad@gmail.com>
2017-03-28 22:29:30 +0300

    modal tmpl

--- js/ref-commands.js
+++ js/ref-commands.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 	var app = {};
-
-    $('#defaultModal').on('hidden.bs.modal', function(){
-        $(this).find('form')[0].reset();
-    });
-
+    // модели
 	app.CommandModel = Backbone.Model.extend({
 		idAttribute: '_id',
 		defaults: {
@@ -12,29 +8,30 @@ $(document).ready(function(){
 		}
 	});
 
+    app.PersonModel = Backbone.Model.extend({
+        idAttribute: '_id',
+        defaults: {
+            name: ''
+        }
+    });
+
+    // коллекции
 	app.CommandCollection = Backbone.Collection.extend({
 		model: app.CommandModel,
 		url: 'http://127.0.0.1:8080/data/commands.json'
 	});
 
-	// app.CommandItemView = Backbone.View.extend({
-	// 	tagName: 'tr',
-	// 	template: _.template($('#tpl_itemcommand').html()),
-    //
-	// 	initialize: function(){
-	// 		this.render();
-	// 	},
-	// 	render: function(){
-	// 		this.$el.html( this.template(this.model.toJSON()) );
-	// 		return this;
-	// 	}
-	// });
+    app.PersonCollection = Backbone.Collection.extend({
+        model: app.PersonModel,
+        url: 'http://127.0.0.1:8080/data/persons.json'
+    });
 
+    // Views
 	app.CommandsView = Backbone.View.extend({
 		el: $("#tbody-commands"),
 		initialize: function() {
 			// this.listenTo(app.commandCollection, 'add', this.addOne);
-
+            app.personCollection.fetch();
 			app.commandCollection.fetch({
 				success: this.fetch_success,
 				error: function(collection, response, options){console.log('error')}
@@ -44,7 +41,7 @@ $(document).ready(function(){
             'click tr': 'click_tr'
 		},
         fetch_success: function (collection, response, options) {
-            console.log(response);
+            // console.log(response);
             table.clear();
             table.rows.add(response);
             table.draw();
@@ -76,8 +73,7 @@ $(document).ready(function(){
             'click li a[action="change"]': 'change'
         },
         add: function () {
-            console.log('control click');
-            $('#defaultModal').modal('show');
+            app.commandModalView.show();
         },
         change: function () {
             var data_row = table.row( $('#tbody-commands tr.bg-cyan') ).data();
@@ -113,19 +109,54 @@ $(document).ready(function(){
                 return;
             }
             var model = app.commandCollection.get(data_row._id);
-            this.template = _.template( $('#tpl_modal_body').html() );
 
-            console.log(this.template(model.toJSON() ));
-
-            $('#defaultModal').find('.modal-body').html( this.template(model.toJSON() ));
-            $('#defaultModal').modal('show');
-            // $('#defaultModal').modal('show');
+            app.commandModalView.show(model);
         }
 
     });
 
+    app.CommandModalView = Backbone.View.extend({
+        el: '#defaultModal',
+        initialize: function () {
+            this.listenTo(app.personCollection, 'add', this.addOnePerson);
+        },
+        show: function (model) {
+            this.$('form')[0].reset();
+            console.log(this.$('select option'));
+            this.$('select option').prop('selected', false);
+            this.$('select').selectpicker('refresh');
+
+            if (model) {
+                this.$('input[name="name"]').val(model.get('name'));
+                this.$('input[name="name"]').parent().addClass('focused');
+                this.$('select[name="persons"]').val('2');
+
+                this.$('select').selectpicker('refresh');
+
+            } else {
+                this.$('input[name="name"]').parent().removeClass('focused')
+            }
+
+            this.$el.modal('show');
+
+        },
+        addOnePerson: function(person) {
+            console.log(person.toJSON());
+
+            this.$('select[name="persons"]').append($("<option></option>")
+                .attr("value", person.get('_id'))
+                .text(person.get('name') ));
+
+            this.$('select[name="persons"]').selectpicker('refresh');
+        }
+    });
+
 	app.commandCollection = new app.CommandCollection();
-	app.commandsView = new app.CommandsView();
+    app.personCollection = new app.PersonCollection();
+
     app.commandControlView = new app.CommandControlView();
+    app.commandModalView = new app.CommandModalView();
+    // всегда последнее так как инициализирует fetch collections
+    app.commandsView = new app.CommandsView();
 
 });
